acd867dacf4dc147d496b16772831ff30e229ba8
Ubuntu <ubuntu@ip-172-31-86-185.ec2.internal>
2019-12-21 20:57:48 +0000

    completed

--- main.js
+++ main.js
@@ -38,9 +38,11 @@ function writeToDocument(type) {
         data.forEach(function(item) {
             var dataRow = [];
          Object.keys(item).forEach(function(key) {
-             dataRow.push(`<td>${item[key]}</td>`);
+             var rowData = item[key].toString();
+             var truncatedData = rowData.substring(0, 15);
+             dataRow.push(`<td>${truncatedData}</td>`);
          });
-         tableRows.push(dataRow);
+         tableRows.push(`<tr>${dataRow}</tr>`);
         });
         
         el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
